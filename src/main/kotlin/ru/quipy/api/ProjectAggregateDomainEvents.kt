880687168a1528623321cb660617993a4b5a479a
Telushkin Artem <artem.telushkin@yandex.ru>
2024-11-01 19:41:43 +0300

    feat: userManagerAggregate(#1,#2)

--- src/main/kotlin/ru/quipy/api/ProjectAggregateDomainEvents.kt
+++ src/main/kotlin/ru/quipy/api/ProjectAggregateDomainEvents.kt
@@ -8,48 +8,86 @@ const val PROJECT_CREATED_EVENT = "PROJECT_CREATED_EVENT"
 const val TAG_CREATED_EVENT = "TAG_CREATED_EVENT"
 const val TAG_ASSIGNED_TO_TASK_EVENT = "TAG_ASSIGNED_TO_TASK_EVENT"
 const val TASK_CREATED_EVENT = "TASK_CREATED_EVENT"
+const val USER_CREATED_EVENT = "USER_CREATED_EVENT"
+const val USER_UPDATED_EVENT = "USER_UPDATED_EVENT"
+const val USER_MANAGER_CREATED_EVENT = "USER_MANAGER_CREATED_EVENT"
 
 // API
 @DomainEvent(name = PROJECT_CREATED_EVENT)
 class ProjectCreatedEvent(
-    val projectId: UUID,
-    val title: String,
-    val creatorId: String,
-    createdAt: Long = System.currentTimeMillis(),
+        val projectId: UUID,
+        val title: String,
+        val creatorId: String,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = PROJECT_CREATED_EVENT,
-    createdAt = createdAt,
+        name = PROJECT_CREATED_EVENT,
+        createdAt = createdAt,
 )
 
 @DomainEvent(name = TAG_CREATED_EVENT)
 class TagCreatedEvent(
-    val projectId: UUID,
-    val tagId: UUID,
-    val tagName: String,
-    createdAt: Long = System.currentTimeMillis(),
+        val projectId: UUID,
+        val tagId: UUID,
+        val tagName: String,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = TAG_CREATED_EVENT,
-    createdAt = createdAt,
+        name = TAG_CREATED_EVENT,
+        createdAt = createdAt,
 )
 
 @DomainEvent(name = TASK_CREATED_EVENT)
 class TaskCreatedEvent(
-    val projectId: UUID,
-    val taskId: UUID,
-    val taskName: String,
-    createdAt: Long = System.currentTimeMillis(),
+        val projectId: UUID,
+        val taskId: UUID,
+        val taskName: String,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = TASK_CREATED_EVENT,
-    createdAt = createdAt
+        name = TASK_CREATED_EVENT,
+        createdAt = createdAt
 )
 
 @DomainEvent(name = TAG_ASSIGNED_TO_TASK_EVENT)
 class TagAssignedToTaskEvent(
-    val projectId: UUID,
-    val taskId: UUID,
-    val tagId: UUID,
-    createdAt: Long = System.currentTimeMillis(),
+        val projectId: UUID,
+        val taskId: UUID,
+        val tagId: UUID,
+        createdAt: Long = System.currentTimeMillis(),
 ) : Event<ProjectAggregate>(
-    name = TAG_ASSIGNED_TO_TASK_EVENT,
-    createdAt = createdAt
+        name = TAG_ASSIGNED_TO_TASK_EVENT,
+        createdAt = createdAt
+)
+
+@DomainEvent(name = USER_CREATED_EVENT)
+class UserCreatedEvent(
+        val userManagerId:UUID,
+        val userId: UUID,
+        val nickname: String,
+        val userName: String,
+        val password: String,
+        createdAt: Long = System.currentTimeMillis()
+) : Event<UserManagerAggregate>(
+        name = USER_CREATED_EVENT,
+        createdAt = createdAt
+)
+
+@DomainEvent(name = USER_UPDATED_EVENT)
+class UserUpdatedEvent(
+        val userManagerId:UUID,
+        val userId: UUID,
+        val nickname: String,
+        val userName: String,
+        val password: String,
+        createdAt: Long = System.currentTimeMillis()
+) : Event<UserManagerAggregate>(
+        name = USER_UPDATED_EVENT,
+        createdAt = createdAt
+)
+
+@DomainEvent(name = USER_MANAGER_CREATED_EVENT)
+class UserManagerCreatedEvent(
+        val userManagerId: UUID,
+        createdAt: Long = System.currentTimeMillis(),
+) : Event<UserManagerAggregate>(
+        name = USER_MANAGER_CREATED_EVENT,
+        createdAt = createdAt
 )
